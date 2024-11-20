1eeed5ddf41217eb8a1ac37f68a0be573c0d743b
Luke Bouch <luke@lukebouch.com>
2024-11-20 18:54:21 -0500

    Clean up default content

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = 'Albemarle Mesh';
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+    'A community project to setup a Meshtastic mesh in Albemarle County, VA';
+export const MY_NAME = 'Luke Bouch';
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
